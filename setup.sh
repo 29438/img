df0e248e02d51a60f3150bcf78b567519939a766
29438 <47177190+29438@users.noreply.github.com>
2021-08-22 22:30:34 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ wget -O code.zip https://cdn.jsdelivr.net/gh/29438/img@master/Cpdaily.zip
 echo "开始解压项目代码"
 unzip -o code.zip
 echo "开始初始化项目依赖"
-pip3 install -r auto-cpdaily-main/requirements.txt -t auto-cpdaily-main/ -i https://mirrors.aliyun.com/pypi/simple
+pip3 install -r FuckCpdaily/requirements.txt -t FuckCpdaily/ -i https://mirrors.aliyun.com/pypi/simple
 if [ -d src/ ];then
   echo "清除原有SRC目录"
   rm -rf src/
 fi
-mv auto-cpdaily-main/ src/
+mv FuckCpdaily/ src/
 rm code.zip
 echo "项目初始化完成，请自行修改src/config.yml内的用户参数"
 echo "不要忘记点击 部署 按钮上传配置！！！"
