ad977bf9372515b4581fa2e13cc6fdcb71a259e2
29438 <47177190+29438@users.noreply.github.com>
2021-09-01 23:41:01 +0800

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,6 @@
 echo "欢迎使用今日校园任务自动化"
 echo "开始下载项目代码"
-wget -O code.zip https://cdn.jsdelivr.net/gh/29438/img@master/FuckCpdaily.zip
+wget -O code.zip https://github.com/29438/auto-cpdaily/archive/refs/heads/main.zip
 echo "开始解压项目代码"
 unzip -o code.zip
 echo "开始初始化项目依赖"
